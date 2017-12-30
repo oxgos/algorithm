9e2e50779386f87b6d86dbe79669d13b855b2082
gjw <122486975@qq.com>
2017-12-30 10:48:48 +0800

    新增归并排序

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var { generateRandomArray } = require('./util/generateRandomArray')
 var { selectSort } = require('./选择排序/selectionSort')
 var { insertSort } = require('./插入排序/insertionSort')
+var { mergeSort } = require('./归并排序/mergeSort')
 var { testSort } = require('./util/testSort')
 
 // 数组个数
-var n = 50000
+var n = 10000
 // var arr2 = []
 
 // 生成数组
-var arr = generateRandomArray(n, 0, 10)
+var arr = generateRandomArray(n, 0, n)
 
 // 复制一个同样的数组,三种方法都行
 // arr2 = arr.map((item) => item)
 // arr2.push(...arr)
 var [...arr2] = arr
+var [...arr3] = arr
 
 testSort(arr, n, selectSort)
 testSort(arr2, n, insertSort)
+testSort(arr3, n, mergeSort)
---
+++ 归并排序/mergeSort.js
@@ -0,0 +1,43 @@
+exports.mergeSort = (arr, n) => {
+    // 把[s, l]的数组归并排序
+    (function _mergeSort(arr, s, l) {
+        if ( s >= l) {
+            return;
+        } else {
+            var mid = Math.floor((s + l) / 2)
+            _mergeSort(arr, s, mid)
+            _mergeSort(arr, mid + 1, l)
+            _merge(arr, s, mid, l)
+        }
+    })(arr, 0, n - 1)
+    
+    function _merge(arr, s, mid, l) {
+        // 新建一个用于储存 ‘需排序的数组’ 的数组
+        // 用于对比‘左边开始的0’与‘右边开始的mid+1’的数字,小的数字放回原数组，并且小的一方索引+1
+        var arrCopy = []
+        for (let i = s; i <= l; i++) {
+            arrCopy.push(arr[i])
+        }
+        var i = s, 
+            j = mid + 1
+
+        for (var k = s; k <= l; k++) {
+            // 判断左右的界限，左边没有数字了，就把右边剩下的全部放回数组，右边同理
+            if (i > mid) {
+                arr[k] = arrCopy[j - s]
+                j++
+            } else if (j > l) {
+                arr[k] = arrCopy[i - s]
+                i++
+            } else {
+                if (arrCopy[i - s] < arrCopy[j - s]) {
+                    arr[k] = arrCopy[i - s]
+                    i++
+                } else {
+                    arr[k] = arrCopy[j - s]
+                    j++
+                }
+            }
+        }
+    }
+}
